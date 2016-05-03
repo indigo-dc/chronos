8c3c4550e27ca48942090889c74b36d07ab14a18
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-05-03 15:41:22 +0200

    Update run.sh

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/run.sh | python
+curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python
 
