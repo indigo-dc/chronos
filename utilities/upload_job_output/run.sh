aae68d413178ff10dc94789868e2524a4a9cafb8
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-05-23 12:03:46 +0000

    Referencing main.py in devel branch

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python
+curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
 
