e5618f3eb8a67a1ac9c7dd372e6a98bc812b63ee
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-05-24 07:50:44 +0000

    Added mountpoint for oneclient

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+oneclient --no_check_certificate --authentication token /mnt/oneclient
+
 curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
 
