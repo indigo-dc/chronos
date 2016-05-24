02e089431020b6dc6769875d87cd9fd9524bc0db
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-05-24 08:10:55 +0000

    Added mount point creation

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir /mnt/oneclient
 oneclient --no_check_certificate --authentication token /mnt/oneclient
 
 curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
