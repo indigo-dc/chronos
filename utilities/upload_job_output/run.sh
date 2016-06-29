c40eb132971ce23fb0d040b3fd5a0d3bf3df4b6c
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-06-29 22:19:04 +0200

    Update run.sh

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -3,5 +3,7 @@
 mkdir /mnt/oneclient
 oneclient --no_check_certificate --authentication token /mnt/oneclient
 
+cd /mnt/oneclient/"$ONEDATA_SPACE"/"$ONEDATA_PATH"
+
 curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
 
