7a6edfa5df4decd9085d86c753cad929ca736955
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-06-29 22:43:21 +0200

    Update run.sh

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+env
+
 mkdir /mnt/oneclient
-ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" oneclient --no_check_certificate --authentication token /mnt/oneclient
+ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token /mnt/oneclient
 
 cd /mnt/oneclient/"$ONEDATA_SPACE"/"$ONEDATA_PATH"
 
