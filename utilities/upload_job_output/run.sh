635ffaec19133716b3e6443737fbd2b972531977
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-06-30 15:02:41 +0200

    Integrate oneclient (#1)
    * FileUploader class alligned to the new tosca templates
    
    * Oneclient installation added

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python
+env
+
+mkdir /mnt/onedata
+ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient /mnt/onedata
+
+cd /mnt/onedata/
+
+export UPLOAD_DIR="$ONEDATA_SPACE"/"$ONEDATA_PATH"
+
+curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
+
+cd / && umount /mnt/onedata
 
