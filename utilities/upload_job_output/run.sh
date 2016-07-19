8ea18fcd66d42e9b83bd2bc1620dd72b044590d2
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-07-19 12:12:44 +0200

    Update run.sh

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -10,10 +10,7 @@ ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" PROVIDER_HOSTNAME="$ONEDA
 cd /mnt/onedata/
 
 export UPLOAD_DIR="$ONEDATA_SPACE"/"$ONEDATA_PATH"
-
-rm -rf "$UPLOAD_DIR"/*
-
-curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python
+curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python || exit 1
 
 echo "Cleaning temporary files"
 rm -rf "$UPLOAD_DIR"/*
