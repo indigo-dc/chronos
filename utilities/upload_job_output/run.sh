f0aa1b15a70215f24912248a0f3d430ddc109d09
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-07-19 09:40:38 +0000

    Clean-up of the service space added

--- utilities/upload_job_output/run.sh
+++ utilities/upload_job_output/run.sh
@@ -2,14 +2,21 @@
 
 env
 
+echo "Mounting Onedata Service Space..."
+
 mkdir /mnt/onedata
-ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient /mnt/onedata
+ONECLIENT_AUTHORIZATION_TOKEN="$ONEDATA_SERVICE_TOKEN" PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient /mnt/onedata || exit 1
 
 cd /mnt/onedata/
 
 export UPLOAD_DIR="$ONEDATA_SPACE"/"$ONEDATA_PATH"
 
-curl -s https://raw.githubusercontent.com/indigo-dc/chronos/devel/utilities/upload_job_output/main.py | python
+rm -rf "$UPLOAD_DIR"/*
+
+curl -s https://raw.githubusercontent.com/indigo-dc/chronos/master/utilities/upload_job_output/main.py | python
+
+echo "Cleaning temporary files"
+rm -rf "$UPLOAD_DIR"/*
 
 cd / && umount /mnt/onedata
 
